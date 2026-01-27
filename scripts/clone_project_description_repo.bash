32a8b07a81c1a06069952eda593548081cf2bfc9
Vlad Krupenko <dev.fixique@gmail.com>
2026-01-27 13:16:48 +0300

    Delete unused scripts

--- scripts/clone_project_description_repo.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-PROJECT_DESCRIPTION_BASE_URL=$(grep -E 'url: ".*/ProjectDescription(\.git)?", branch: "' Package.swift | sed -nE 's|.*url: "(.*)/ProjectDescription(\.git)?".*|\1|p')
-PROJECT_DESCRIPTION_VERSION=$(grep -E 'url: ".*/ProjectDescription(\.git)?", branch: "' Package.swift | sed -n 's/.*branch: "\([^"]*\)".*/\1/p')
-if [ -z "$PROJECT_DESCRIPTION_VERSION" ]; then
-    echo "Error: PROJECT_DESCRIPTION_VERSION not found"
-    exit 1
-fi
-git clone --depth 1 --branch $PROJECT_DESCRIPTION_VERSION "$PROJECT_DESCRIPTION_BASE_URL/ProjectDescription"
