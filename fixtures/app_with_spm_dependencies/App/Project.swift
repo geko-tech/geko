cd3956f01b33f6de3095152fd172fa392e640af5
Vlad Krupenko <dev.fixique@gmail.com>
2026-01-27 19:09:06 +0300

    Update fixture

--- fixtures/app_with_spm_dependencies/App/Project.swift
+++ fixtures/app_with_spm_dependencies/App/Project.swift
@@ -2,62 +2,62 @@
 import ProjectDescriptionHelpers
 
 let project = Project(
-    name: "App",
-    settings: .projectSettings,
-    targets: [
+   name: "App",
+   settings: .projectSettings,
+   targets: [
+       Target(
+           name: "App",
+           destinations: .iOS,
+           product: .app,
+           bundleId: "io.geko.app",
+           infoPlist: .default,
+           sources: "Sources/App/**",
+           dependencies: [
+               .target(name: "MyAppKit"),
+               .project(target: "FeatureOneFramework_iOS", path: .relativeToRoot("Features/FeatureOne")),
+               .external(name: "Styles"),
+           ],
+           settings: .targetSettings
+       ),
+       Target(
+           name: "MyAppKit",
+           destinations: .iOS,
+           product: .staticFramework,
+           bundleId: "io.geko.app.kit",
+           infoPlist: .default,
+           sources: "Sources/AppKit/**",
+           dependencies: [
+               .sdk(name: "c++", type: .library, status: .required),
+               .external(name: "Alamofire"),
+               .external(name: "ComposableArchitecture"),
+           ],
+           settings: .targetSettings
+       ),
         Target(
-            name: "App",
-            destinations: .iOS,
-            product: .app,
-            bundleId: "io.geko.app",
-            infoPlist: .default,
-            sources: "Sources/App/**",
+            name: "WatchApp",
+            destinations: .watchOS,
+            product: .watch2App,
+            bundleId: "io.geko.app.watchapp",
+            infoPlist: .extendingDefault(
+                with: [
+                    "WKCompanionAppBundleIdentifier": "io.geko.app"
+                ]
+            ),
+            sources: ["Sources/Watch/App/**"],
             dependencies: [
-                .target(name: "MyAppKit"),
-                .project(target: "FeatureOneFramework_iOS", path: .relativeToRoot("Features/FeatureOne")),
-                .external(name: "Styles"),
-            ],
-            settings: .targetSettings
+                .target(name: "WatchExtension")
+            ]
         ),
         Target(
-            name: "MyAppKit",
-            destinations: .iOS,
-            product: .staticFramework,
-            bundleId: "io.geko.app.kit",
-            infoPlist: .default,
-            sources: "Sources/AppKit/**",
+            name: "WatchExtension",
+            destinations: .watchOS,
+            product: .watch2Extension,
+            bundleId: "io.geko.app.watchapp.extension",
+            sources: ["Sources/Watch/Extension/**"],
             dependencies: [
-                .sdk(name: "c++", type: .library, status: .required),
-                .external(name: "Alamofire"),
-                .external(name: "ComposableArchitecture"),
-            ],
-            settings: .targetSettings
+                .external(name: "Alamofire")
+            ]
         ),
-         Target(
-             name: "WatchApp",
-             destinations: .watchOS,
-             product: .watch2App,
-             bundleId: "io.geko.app.watchapp",
-             infoPlist: .extendingDefault(
-                 with: [
-                     "WKCompanionAppBundleIdentifier": "io.geko.app"
-                 ]
-             ),
-             sources: ["Sources/Watch/App/**"],
-             dependencies: [
-                 .target(name: "WatchExtension")
-             ]
-         ),
-         Target(
-             name: "WatchExtension",
-             destinations: .watchOS,
-             product: .watch2Extension,
-             bundleId: "io.geko.app.watchapp.extension",
-             sources: ["Sources/Watch/Extension/**"],
-             dependencies: [
-                 .external(name: "Alamofire")
-             ]
-         ),
-    ],
-    schemes: Scheme.allSchemes(for: ["App", "MyAppKit"], executable: "App")
+   ],
+   schemes: Scheme.allSchemes(for: ["App", "MyAppKit"], executable: "App")
 )
