4fceb37c54e3d475a689cf8cc93c1b55c34698ed
t.taymasov <t.taymasov@ttaymasovs-Mac-mini-3.local>
2026-07-16 02:02:46 +0300

    geko [none] test: cover generate plan modes

--- Tests/GekoKitTests/Commands/GenerateCommandTests.swift
+++ Tests/GekoKitTests/Commands/GenerateCommandTests.swift
@@ -6,6 +6,15 @@ final class GenerateCommandTests: XCTestCase {
     func test_parse_acceptsPlanOption() throws {
         let command = try GenerateCommand.parse(["--plan", "focus.plan"])
 
+        XCTAssertFalse(command.cache)
+        XCTAssertEqual(command.options.plan, "focus.plan")
+        XCTAssertEqual(command.options.sources, [])
+    }
+
+    func test_parse_acceptsCacheWithPlanOption() throws {
+        let command = try GenerateCommand.parse(["--cache", "--plan", "focus.plan"])
+
+        XCTAssertTrue(command.cache)
         XCTAssertEqual(command.options.plan, "focus.plan")
         XCTAssertEqual(command.options.sources, [])
     }
